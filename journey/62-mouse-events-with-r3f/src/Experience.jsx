eadbd56d5845e6657f1e752c9178d94d9de63981
harry <koler778@gmail.com>
2025-08-26 09:14:31 +0800

    feat: create 62-mouse-events-with-r3f journey with Vite and React Three Fiber, featuring interactive 3D elements, mouse event handling, and a custom GLB model for enhanced user engagement

---
+++ journey/62-mouse-events-with-r3f/src/Experience.jsx
@@ -0,0 +1,59 @@
+import { useFrame } from '@react-three/fiber';
+import { OrbitControls, useGLTF, meshBounds} from '@react-three/drei';
+import { useRef } from 'react';
+
+export default function Experience() {
+  const cube = useRef();
+  const hamburger = useGLTF('./hamburger.glb');
+
+  useFrame((state, delta) => {
+    cube.current.rotation.y += delta * 0.2;
+  });
+
+  const eventHandler = (event) => {
+    cube.current.material.color.set(`hsl(${Math.random() * 360}, 100%, 75%)`);
+  };
+
+  return (
+    <>
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      <mesh position-x={-2} onClick={(e) => e.stopPropagation()}>
+        <sphereGeometry />
+        <meshStandardMaterial color='orange' />
+      </mesh>
+
+      <mesh
+        ref={cube}
+        position-x={2}
+        scale={1.5}
+        onClick={eventHandler}
+        onPointerEnter={() => (document.body.style.cursor = 'pointer')}
+        onPointerLeave={() => (document.body.style.cursor = 'default')}
+        raycast={meshBounds}
+      >
+        <boxGeometry />
+        <meshStandardMaterial color='mediumpurple' />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color='greenyellow' />
+      </mesh>
+
+      <primitive
+        object={hamburger.scene}
+        position-y={0.5}
+        scale={0.25}
+        onClick={(event) => {
+          console.log('event.object', event.object);
+          console.log('event.eventObject', event.eventObject);
+          event.stopPropagation();
+        }}
+      />
+    </>
+  );
+}
