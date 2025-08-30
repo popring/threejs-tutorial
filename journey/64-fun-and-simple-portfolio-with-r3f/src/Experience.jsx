2004cb3e4b3af60e12eb745f22ca410e20afd69e
harry <koler778@gmail.com>
2025-08-30 23:35:53 +0800

    feat: create 62-mouse-events-with-r3f journey with Vite and React Three Fiber, featuring interactive 3D elements, mouse event handling, and a custom GLB model for enhanced user engagement

---
+++ journey/64-fun-and-simple-portfolio-with-r3f/src/Experience.jsx
@@ -0,0 +1,36 @@
+import {
+  Environment,
+  Float,
+  OrbitControls,
+  PresentationControls,
+  useGLTF,
+} from '@react-three/drei';
+
+export default function Experience() {
+  const computer = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+  );
+
+  return (
+    <>
+      <color args={['#241a1a']} attach={'background'} />
+
+      <Environment preset='city' />
+
+      {/* <OrbitControls makeDefault /> */}
+
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap
+      >
+        <Float rotationIntensity={0.4}>
+          <primitive object={computer.scene} position-y={-1.2} />
+        </Float>
+      </PresentationControls>
+    </>
+  );
+}
